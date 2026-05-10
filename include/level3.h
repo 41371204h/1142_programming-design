aa0712480d89d81952e598964d2e6f46d66a8261
Jane Hsu <jenling.hsu@gmail.com>
2026-05-10 22:05:39 +0800

    Create a new file include/level3.h

---
+++ include/level3.h
@@ -0,0 +1,11 @@
+#ifdef LEVEL3_H
+#define LEVEL3_H
+
+#include "game_shared.h"
+
+// to modify the state of the game for level 3
+void UpdateLevel3(GameState *state);
+// Draw everything for level 3 e.g., maze, player, UI, etc.
+void DrawLevel3(void);
+
+#endif // LEVEL3_H

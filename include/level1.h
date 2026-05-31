e56ab2bd9ff9cca156b3d4decd43fcc0a614f2ef
yungching-tsai <c844214@gmail.com>
2026-05-31 21:03:51 +0800

    Add files via upload

---
+++ include/level1.h
@@ -0,0 +1,19 @@
+#ifndef LEVEL1_H
+#define LEVEL1_H
+
+#include "game_shared.h"
+
+// initialize level 1 variables
+void InitLevel1(void);
+
+// Update level 1 logic: receive state from main_hub and conduct item/status modification
+void UpdateLevel1(GameState *state);
+
+// 繪製第一關畫面 (接收大腦傳來的 state 讀取 Q 版人物貼圖)
+void DrawLevel1(const GameState *state);
+
+void DrawPaperText(void);
+void DrawMorseTable(void);
+void DrawNavigationCommand(void);
+
+#endif // LEVEL1_H

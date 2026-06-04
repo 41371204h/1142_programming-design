e72e70cd2cb3d33614ef01e4f0c5efaa9b8384dc
41371204h <41371204h@gapps.ntnu.edu.tw>
2026-06-04 19:20:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 mkdir -p build
 
 # 執行 GCC 編譯
-gcc src/*.c -o build/game_exe -Iinclude -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+gcc src/*.c -o build/game_exe -Iinclude -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -lSDL2 -lSDL2_mixer
 
 if [ $? -eq 0 ]; then
     echo "編譯成功！啟動遊戲..."

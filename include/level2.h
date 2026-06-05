3646f1e1392e4d5632eb34e363eb365f873139c5
yungching-tsai <c844214@gmail.com>
2026-06-05 15:18:56 +0800

    Add DrawCompletedMap function declaration

--- include/level2.h
+++ include/level2.h
@@ -1,8 +1,11 @@
 #ifndef LEVEL2_H
 #define LEVEL2_H
 
+#include "game_shared.h"
+
 void InitLevel2(void);
 void UpdateLevel2(GameState *state);
 void DrawLevel2(const GameState *state);
+void DrawCompletedMap(void);
 
 #endif

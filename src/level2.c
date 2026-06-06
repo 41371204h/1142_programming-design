42e3043b7a4615973679b9a3986ef944a3977ceb
yungching-tsai <c844214@gmail.com>
2026-06-06 13:53:08 +0800

    將拼圖塊分布方式改為正式版

--- src/level2.c
+++ src/level2.c
@@ -105,15 +105,17 @@ static void AdvanceDialogue(GameState *state)
 void InitPuzzle(void)
 {
     int temp[9] = {
+        /* 測試版
         2,1,3,
         4,5,6,
         7,8,9
+        */
 
-        /* 正式版
+        // /* 正式版
         4,7,2,
         8,3,5,
         1,9,6
-        */
+        // */
     };
 
     for (int i = 0; i < 9; i++)
